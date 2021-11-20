25ba10829e3ed039096139bd09d4985bc6072f7b
ertovkax <s280906k@gmail.com>
2021-11-20 18:35:36 +0300

    Updates

---
+++ precache-manifest.231d55c88f8628b49f5f7624a736b328.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c09f91e2f1f06fa40cef3998a569d93",
+    "url": "/coinhave/index.html"
+  },
+  {
+    "revision": "7878153437d12ad31b39",
+    "url": "/coinhave/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "9672509f265c5bde48f9",
+    "url": "/coinhave/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "7878153437d12ad31b39",
+    "url": "/coinhave/static/js/2.31146829.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/coinhave/static/js/2.31146829.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9672509f265c5bde48f9",
+    "url": "/coinhave/static/js/main.c9fee35b.chunk.js"
+  },
+  {
+    "revision": "675cde629c15a4129785",
+    "url": "/coinhave/static/js/runtime-main.fe797d87.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/coinhave/static/media/persik.4e1ec840.png"
+  }
+]);
